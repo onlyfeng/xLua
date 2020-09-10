af97826bb272a55fbf5b1f95e9c7e5c5c4eeac19
onlyfeng <onlyfeng@gmail.com>
2020-09-10 18:10:39 +0800

    同步 PLATFORM 设置

--- build/make_ios_lua53.sh
+++ build/make_ios_lua53.sh
@@ -1,5 +1,5 @@
 mkdir -p build_ios && cd build_ios
-cmake -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -GXcode ../
+cmake -DLUAC_COMPATIBLE_FORMAT=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=OS64 -GXcode ../
 cd ..
 cmake --build build_ios --config Release
 mkdir -p plugin_lua53/Plugins/iOS/
