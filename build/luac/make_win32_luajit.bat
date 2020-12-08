5cc6e3c9c894ce671ba560dca6e59e48e2b4b6d7
onlyfeng <onlyfeng@gmail.com>
2020-12-08 22:01:24 +0800

    luac 增加编译发布 luajit window平台执行脚本

---
+++ build/luac/make_win32_luajit.bat
@@ -0,0 +1,14 @@
+@echo off
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+
+echo Swtich to x86 build env
+cd %~dp0\luajit2\src
+call msvcbuild.bat static
+cd ..\..
+
+md luac\bin_luajit\32
+copy /Y luajit2\src\luajit.exe luac\bin_luajit\32\luajit.exe
+xcopy /I /E /Y luajit2\src\jit luac\bin_luajit\32\jit
+del /Q luac\bin_luajit\32\jit\.gitignore
+pause
---
+++ build/luac/make_win64_luajit.bat
@@ -0,0 +1,13 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+echo Swtich to x64 build env
+cd %~dp0\luajit2\src
+call msvcbuild.bat gc64 static
+cd ..\..
+
+md luac\bin_luajit\64
+copy /Y luajit2\src\luajit.exe luac\bin_luajit\64\luajit.exe
+xcopy /I /E /Y luajit2\src\jit luac\bin_luajit\64\jit
+del /Q luac\bin_luajit\64\jit\.gitignore
+pause
